873e18cc57bc01e8454a3d74ef052e27266c627a
Andrea Gorletta <andrea.alice.gorletta@gmail.com>
2025-01-10 17:52:59 +0100

    ffs

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import history from "connect-history-api-fallback";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "configure-server",
+      configureServer(server) {
+        server.middlewares.use(
+          history({
+            disableDotRule: true,
+            htmlAcceptHeaders: ["text/html", "application/xhtml+xml"],
+          })
+        );
+      },
+    },
+  ],
+  server: {
+    host: "0.0.0.0", // Expose the server to the network
+  },
+});
