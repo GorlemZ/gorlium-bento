424cd0deff86bee09e6fdf064ae582058a824317
Andrea Gorletta <andrea.alice.gorletta@gmail.com>
2025-01-31 15:52:03 +0100

    github icon added

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,26 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import {
-  BentoProvider,
-  Box,
-  defaultMessages,
-} from "@gorliumbento/design-system";
+import { BentoProvider, defaultMessages } from "@gorliumbento/design-system";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BentoProvider defaultMessages={defaultMessages}>
-      <Box
-        style={{
-          backgroundColor: "#101401",
-
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh",
-        }}
-      >
-        <App />
-      </Box>
+      <App />
     </BentoProvider>
   </StrictMode>
 );
